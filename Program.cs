cf653f19cdd04815c97b16023914b6f4236f9b4f
Hokutosei <jeanepaul@gmail.com>
2017-04-12 02:28:10 +0900

    init

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using Docker.DotNet;
+using Docker.DotNet.Models;
+
+namespace faas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DockerClient client = new DockerClientConfiguration(new Uri("unix:///var/run/docker.sock"))
+     .CreateClient();
+
+            listContainers(client);
+            Console.WriteLine($"done!");
+            Thread.Sleep(300000);
+        }
+
+        public static async void listContainers(DockerClient client)
+        {
+            Console.WriteLine($"exec");
+            var p = new ContainersListParameters() {
+                Limit = 10
+            };
+            var containers = await client.Containers.ListContainersAsync(p);
+            Console.WriteLine($"containers {containers}");
+            foreach(var container in containers)
+            {
+                Console.WriteLine($"container {container.ID}");
+            }
+
+            return;
+        }
+    }
+}
